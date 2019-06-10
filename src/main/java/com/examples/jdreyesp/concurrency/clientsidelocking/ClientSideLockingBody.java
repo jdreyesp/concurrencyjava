461c9d6644dfa85a409a53b7adba89e614dc685b
jdreyesp <jdiego.reyes@gmail.com>
2019-06-10 20:14:08 +0200

    Added ClientSideLocking + Test

---
+++ src/main/java/com/examples/jdreyesp/concurrency/clientsidelocking/ClientSideLockingBody.java
@@ -0,0 +1,14 @@
+package com.examples.jdreyesp.concurrency.clientsidelocking;
+
+public class ClientSideLockingBody {
+
+    private String element;
+
+    public String getElement() {
+        return element;
+    }
+
+    public void setElement(String element) {
+        this.element = element;
+    }
+}
---
+++ src/test/java/com/examples/jdreyesp/concurrency/clientsidelocking/ClientSideLockingControllerTest.java
@@ -0,0 +1,81 @@
+package com.examples.jdreyesp.concurrency.clientsidelocking;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * We assure in this test that the list is well managed by the client lock
+ */
+public class ClientSideLockingControllerTest {
+
+    private static final ExecutorService pool = Executors.newCachedThreadPool();
+
+    private static final int NUM_PRODUCERS = 100;
+    private static final int NUM_CONSUMERS = 100;
+
+    private CyclicBarrier barrier = new CyclicBarrier(NUM_CONSUMERS + NUM_PRODUCERS + 1);
+
+    private ClientSideLockingController sut = new ClientSideLockingController();
+
+    @Test
+    public void shouldBehaveWellOnConcurrentProducersAndConsumers() throws BrokenBarrierException, InterruptedException {
+
+        for(int i = 0; i< NUM_PRODUCERS; i++) {
+            pool.execute(new Producer());
+        }
+
+        for(int i = 0; i< NUM_PRODUCERS; i++) {
+            pool.execute(new Consumer());
+        }
+
+        System.out.println("Awaiting for producers and consumers to be ready");
+        barrier.await(); //Wait for all threads to be ready
+        barrier.await(); //Wait for all threads to finish
+        System.out.println(String.format("Generated list: %s", Arrays.toString(sut.getElements().toArray())));
+        assertEquals(100, sut.getElements().size());
+    }
+
+    private class Producer implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(1000);
+                final ClientSideLockingBody body = new ClientSideLockingBody();
+                String element = Thread.currentThread().getName();
+                body.setElement(element);
+
+                System.out.println(String.format("Producer %s ready", element));
+                barrier.await();
+                sut.putOnList(body);
+                barrier.await();
+            } catch (InterruptedException | BrokenBarrierException | ClientSideLockingController.AlreadyOnListException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private class Consumer implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(1000);
+                String element = Thread.currentThread().getName();
+                System.out.println(String.format("Consumer %s ready", element));
+                barrier.await();
+                sut.getElements();
+                barrier.await();
+            } catch (InterruptedException | BrokenBarrierException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
